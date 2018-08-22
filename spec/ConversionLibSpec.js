4b4ee944dda58dc59b35e02f8319dba94c462fa1
Rainer Rillke <rillke@wikipedia.de>
2018-08-22 09:29:32 +0200

    Added unit tests. npm test. Jasmine.

---
+++ spec/ConversionLibSpec.js
@@ -0,0 +1,43 @@
+/* jshint node: true, esversion: 6 */
+describe("lib-convert", function() {
+  var srcConvert = require('../src/lib-convert');
+  var libConvert = require('../lib/lib-convert');
+  var fs = require('fs');
+  var fritzXML = fs.readFileSync(__dirname + '/fritz-phonebook.xml',
+    'utf8');
+  var fritzJSON = fs.readFileSync(__dirname +
+    '/fritz-phonebook.json', 'utf8');
+  var fritzVCard = fs.readFileSync(__dirname +
+    '/fritz-phonebook.vcard.json', 'utf8');
+  var XML;
+  var vCard;
+
+  it('pixl-xml', function() {
+    var parsedXML;
+    XML = require('pixl-xml');
+    expect(XML.parse).toEqual(jasmine.any(Function));
+    expect(parsedXML = XML.parse(fritzXML)).toEqual(jasmine.any(
+      Object));
+    expect(JSON.parse(fritzJSON)).toEqual(parsedXML);
+  });
+
+  it('lib-convert', function() {
+    expect(srcConvert.fritzXML2vcard).toEqual(jasmine.any(Function));
+    expect(libConvert.fritzXML2vcard).toEqual(jasmine.any(Function));
+
+    var vCardsFromSrc, vCardsFromLib;
+    expect(vCardsFromSrc = srcConvert.fritzXML2vcard(fritzXML)).toEqual(
+      jasmine.any(Object));
+    expect(vCardsFromLib = libConvert.fritzXML2vcard(fritzXML)).toEqual(
+      jasmine.any(Object));
+
+    vCardsFromSrc = JSON.parse(JSON.stringify(vCardsFromSrc).replace(
+      /REV:[^"]+/g, 'REV:'));
+    vCardsFromLib = JSON.parse(JSON.stringify(vCardsFromLib).replace(
+      /REV:[^"]+/g, 'REV:'));
+    fritzVCard = JSON.parse(fritzVCard.replace(/REV:[^"]+/g, 'REV:'));
+
+    expect(fritzVCard).toEqual(vCardsFromSrc);
+    expect(fritzVCard).toEqual(vCardsFromLib);
+  });
+});
